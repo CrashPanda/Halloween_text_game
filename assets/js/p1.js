dd94bc5e9674ba373694cb1bad453a78861cd5d4
CrashPanda <samdaize@hotmail.com>
2022-06-12 13:29:30 +0100

    Update p1.js

--- assets/js/p1.js
+++ assets/js/p1.js
@@ -1,9 +1,9 @@
 function hideaway1() {
-    document.getElementById('hideaway1').style.visibility='visible';
+    document.getElementById('hideaway1').style.display='block'; document.getElementById('hideaway').style.display='none';
 }
 
 function hideaway2() {
-    document.getElementById('hideaway2').style.visibility='visible';
+    document.getElementById('hideaway2').style.display='block';
 }
 
 
